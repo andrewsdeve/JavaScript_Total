1d05698316ada3ac4bf0b69cea16391bb09c8043
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-02-05 19:39:55 -0500

    dia4_ver1

--- Dia_4/3-Calcular_Combustible/dev.js
+++ Dia_4/3-Calcular_Combustible/dev.js
@@ -3,7 +3,7 @@ function calcularLitros(){
     let textKm = elementoKm.value;
     let cantKm = Number(textKm);
 
-    let cantLitros = cantKm / 8.8;
+    let cantLitros =Math.ceil(cantKm / 8.8);
     let resultado = document.getElementById("textoResultado");
 
     resultado.textContent = `Carga ${cantLitros} litros de combustible`
