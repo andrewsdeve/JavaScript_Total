d17c333a635ae9209b41cf6c3e8ad126aa7265fa
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-02-13 08:52:46 -0500

    Ejercicio_Practico_2

---
+++ Dia_6/3-Loop_For_2/practica_1.js
@@ -0,0 +1,8 @@
+let suma = 0;
+
+function testLoop(){
+    for(let x =1 ; x<=5 ; x++){
+        suma = suma + x;
+        console.log("El valor de X es: "+ suma);
+    }    
+}
---
+++ Dia_6/3-Loop_For_2/practica_2.js
@@ -0,0 +1,6 @@
+function testLoop(arg){
+    for(let i=1;i<=10;i++){
+        let multiplicar = arg  * i;
+        console.log(arg  + " x " + i + " = "+multiplicar);
+    }    
+}       
