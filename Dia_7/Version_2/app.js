4d6bb0a44f639fcf41197aa1236e23d70096687d
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-02-22 18:59:32 -0500

    version2.0

--- Dia_7/Version_2/app.js
+++ Dia_7/Version_2/app.js
@@ -8,17 +8,55 @@ function extraerNumero(inputId){
 
 
 function calcular(){
-    let ventas1, ventas2,ventas3,ventas4,ventas5,ventas6;
-    ventas1 = extraerNumero("ventasTienda1");
-    ventas2 = extraerNumero("ventasTienda2");
-    ventas3 = extraerNumero("ventasTienda3");
-    ventas4 = extraerNumero("ventasTienda4");
-    ventas5 = extraerNumero("ventasTienda5");
-    ventas6 = extraerNumero("ventasTienda6");
+    let ventas =[];
+    ventas[0] = extraerNumero("ventasTienda1");
+    ventas[1] = extraerNumero("ventasTienda2");
+    ventas[2] = extraerNumero("ventasTienda3");
+    ventas[3] = extraerNumero("ventasTienda4");
+    ventas[4] = extraerNumero("ventasTienda5");
+    ventas[5] = extraerNumero("ventasTienda6");
 
-    let totalVentas = ventas1 + ventas2 + ventas3 + ventas4 + ventas5 + ventas6;
-    let mensajeSalida = "Total ventas: " + totalVentas;
+    let totalVentas = sumarTotal(ventas)
+    let ventaMayor = calcularMayor(ventas);
+    let ventaMenor = calcularMenor(ventas) ;
 
+
+    let mensajePantalla = `Total Ventas:  ${totalVentas}   Venta Mayor: ${ventaMayor}    Venta Menor:  ${ventaMenor}`
+
+
+    let mensajeSalida = "Total ventas: " + totalVentas +
+                        "/Venta Mayor: " + ventaMayor +
+                        "/Venta Menor: " + ventaMenor;
     let elementoSalida = document.getElementById("parrafoSalida")
-    elementoSalida.textContent = mensajeSalida;
+    elementoSalida.textContent = mensajePantalla;
+}
+
+function sumarTotal(array){
+    let total = 0;
+    for(let venta of array){
+        total = total + venta;
+    }
+    return total
+
+}
+
+
+function calcularMayor(array){
+    let maximo = array[0];
+    for(let venta of array){
+        if(venta > maximo){
+            maximo = venta;
+        }
+    }
+    return maximo
+}
+
+function calcularMenor(array){
+    let minimo = array[0];
+    for(let venta of array){
+        if(venta < minimo){
+            minimo = venta;
+        }
+    }
+    return minimo
 }
