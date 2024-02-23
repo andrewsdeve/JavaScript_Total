7cb6542c70aab80c6a36dbb133dab9156bfe51be
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-02-22 20:54:05 -0500

    version4

---
+++ Dia_7/Version_4/ejercicio1.js
@@ -0,0 +1,7 @@
+/* En el archivo script.js, programe la función generarContenido() que agregue elementos HTML mediante código JavaScript. Se debe agregar, en el div con ID contenido, una etiqueta <p> con el texto "Parrafo 1" y además agregar una etiqueta <input> con el ID txtInput y el atributo type igual a text */
+
+/* Adapta la función generarContenido, para que utilizando el parámetro numero, el input que se genera (y su correspondiente label asociado) utilice este parámetro para definir su ID. Es decir, que si el parámetro numero es 3, el ID del input generado deberá ser txtInput3 y además el label se debe asociar a ese ID del input y el texto que muestra debe decir Label 3.
+
+El objetivo de esto, sería que luego se pueda generar código de forma repetitiva como se vio en la última clase y que los nombres de los campos se generen de forma dinámica. */
+
+/* Implementar la función repetirContenido, para que dado el parámetro veces genere esa misma cantidad de elemento llamando a la función generarContenido ya declarada. Es decir que si el parámetro veces es igual a 5, se deberán generar (mediante la función generarContenido) 5 elementos input con sus respectivos label. */
