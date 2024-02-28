051cc92e335c82c5a02161e1aad2acd7759346b9
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-02-28 17:21:04 -0500

    Practicas

---
+++ Dia_8/PracticasObjetos/app.js
@@ -0,0 +1,13 @@
+/* Dado el objeto automóvil ya creado, implementar la función modificarAnio, que modifique el año del objeto por el año 2018.  */
+
+let automovil = {
+    marca: "Toyota",
+    modelo: "Camry",
+    anio: 2015,
+    color: "Negro",
+    velocidadMaxima: 18
+}
+
+function modificarAnio(){
+    marca.anio = 2018;
+}
