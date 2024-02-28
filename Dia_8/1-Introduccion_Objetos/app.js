255a83cf397e4d50bd0d4513f9952745c790e238
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-02-28 16:53:41 -0500

    IntroduccionObjetos

---
+++ Dia_8/1-Introduccion_Objetos/app.js
@@ -0,0 +1,24 @@
+/* OBJETOS EN JavaScript, cajas donde podemos guardar cosas */
+/* INTRODUCCION: javascript es un lenguaje de progracion basado en objetos,
+let pelota = {
+    color: "azul",
+    tamaño: 10,
+        rebotar(){
+            console.log("La pelota reboa una vez")  => a esta parte se le llama metodo
+        }
+}
+pelota.color => asi accedemos al contenido de las propiedades de los metodos
+perro["edad"] => nos muestra igual la propieda de ese objeto
+
+
+*/
+
+let perro = {
+    nombre :"Roberto",
+    raza:"Labrador",
+    edad:5,
+    peso:20,
+    ladrar(){
+        console.log("Guau Guau!");
+    }
+};
