a3938644290d445e30ec5312088e4541ef8a44f3
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-05 04:52:45 -0500

    EjerciciosUdemy

--- Dia_10/9-Ejercicios/ejercicio2.js
+++ Dia_10/9-Ejercicios/ejercicio2.js
@@ -31,3 +31,46 @@ class EmpleadoPlanta extends Empleado{
 
 let newEmpleado = new Empleado("Juan","Gomez",34);
 let newEmpleadoPlanta = new EmpleadoPlanta("Libardo","Garcia",54,"Reciclaje Carton")
+
+
+// EJERCICIOS SUBCLASES UDEMY 
+/* Dada la clase Persona ya definida, crear la subclase Empleado (que hereda de Persona) pero que agrega la propiedad: legajo. */
+
+class Persona {
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;s
+    }
+}
+
+class Empleado extends Persona{
+    constructor(nombre,apellido,legajo){
+        super(nombre,apellido);
+        this.legajo = legajo;
+    }
+}
+
+let newPersona = new Persona("Juan", "Quintero");
+let newEmpleado1 = new Empleado("Ricardo","Garcia",2345678);
+
+
+/* Dada la clase Medico ya definida, crear la subclase Cirujano (que hereda de Medico) pero que agrega la propiedad: cantOperaciones. */
+
+class Medico {
+    constructor(nombre,apellido,matricula){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matricula = matricula;
+    }
+}
+
+class Cirujano extends Medico{
+    constructor(nombre,apellido,matricula,cantOperaciones){
+        super(nombre,apellido,matricula);
+        this.cantOperaciones = cantOperaciones;
+    }
+}
+
+let newMedico = new Medico("Juan","Rueda",9087654);
+let newCirujano = new Cirujano("Ricardo", "Rico", 2345656 , 45);
+
