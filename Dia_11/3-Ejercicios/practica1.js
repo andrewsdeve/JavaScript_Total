e19aa39c04373383b807fe495ba3fc686e7c791c
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-06 05:06:12 -0500

    FinEjercicio

--- Dia_11/3-Ejercicios/practica1.js
+++ Dia_11/3-Ejercicios/practica1.js
@@ -1,15 +1,27 @@
-let datosJson;
-let xhr = new XMLHttpRequest();
-xhr.open('GET', "practica1.json",true);
-xhr.responseType = 'json';
-xhr.onload = function(){
-    if(xhr.status === 200){
-        datosJson = xhr.response;
-        console.log("Se han cargado los datos");
-        let textoPantalla = document.getElementById("mostrar");
-        textoPantalla.textContent = datosJson.director
-
+let titulo;
+let director;
+let lanzamiento;
+let duracion;
+let actores;
+let premioOscar;
+ 
+function obtenerDatos() {
+    let datosJson;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', "datos.json", true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        if(xhr.status === 200) {
+            datosJson = xhr.response;
+            titulo = datosJson.titulo;
+            director = datosJson.director;
+            lanzamiento = datosJson.lanzamiento;
+            duracion = datosJson.duracion;
+            actores = datosJson.actores;
+            premioOscar = datosJson.premioOscar;
+            let elementoTexto = document.getElementById("nombre");
+                elementoTexto.textContent = datosJson;
+        }
     }
+    xhr.send();
 }
-
-xhr.send();
