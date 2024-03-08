9087a886db6e650f758fcdd47cf50dc58695c37f
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-08 05:04:00 -0500

    JS

---
+++ Dia_11/12-RepasoDia11/app.js
@@ -0,0 +1,44 @@
+/* INTRODUCCION A JSON
+1. JavaScript Object Notation
+2. Puede ser utilizado independientemente que javaScript 
+3. Un ejemplo de json es:
+    {
+        "clave":"valor",
+        "clave1": 100,
+        "clave2": {
+            "clave2-a": "Valorx",
+            "clave2-b": "valory"
+        }
+    }
+
+*/
+
+/* Escriba en formato JSON (dentro de los corchetes de la variable json1), la siguiente información sobre una película:
+
+    titulo -> Titanic
+
+    director -> James Cameron
+
+    lanzamiento -> 1997
+
+    duracion -> 134
+
+    actores (array) -> Leonardo DiCaprio y Kate Winslet
+
+    premioOscar -> true */
+
+    /*
+    Escriba en formato JSON (dentro de los corchetes de la variable json1), la siguiente información sobre un colegio:
+
+    nombre-> Collegium
+
+    direccion (objeto)
+
+        calle -> Calle Publica 123
+
+        ciudad -> Buenos Aires
+
+    cantEstudiantes -> 3
+
+    estudiantes (array) -> Pedro Picapiedra, Pablo Marmol y Vilma Picapiedra */
+
