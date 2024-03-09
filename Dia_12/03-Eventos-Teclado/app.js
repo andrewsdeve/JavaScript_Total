2175fe506953dc45a5be51321382877f86dca14e
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-08 20:09:52 -0500

    EjerciciosEventosTeclado

--- Dia_12/03-Eventos-Teclado/app.js
+++ Dia_12/03-Eventos-Teclado/app.js
@@ -19,3 +19,15 @@ campo.addEventListener('keyup', function(){
 campo.addEventListener('keypress', function(event){
     console.log("Caracter ingresado: " + event.key)
 })
+
+/* Eejerciciso udemy eventos teclado # 1 */
+/* Dado el código HTML proporcionado, programar la función agregarEvento() del archivo index.js, que agregue un evento keypress al input miInput. Al disparar el evento, deberá imprimir en consola el código de la tecla presionada utilizando el elemento key de la variable event. */
+
+
+function agregarEvento() {
+    let input = document.getElementById("miInput");
+    
+    input.addEventListener('keypress', function(event) {
+        console.log("Dato ingresado: " + event.key)
+    });
+}
