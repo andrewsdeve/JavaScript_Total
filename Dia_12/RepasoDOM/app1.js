66943107597609721cc4cbc07267cd2815ad2741
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-09 05:54:21 -0500

    Atributos

---
+++ Dia_12/RepasoDOM/app1.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector('#wrapper');
+const title = document.querySelectorAll('.title');
+const link = document.querySelector('.link.bold');
+const input = document.querySelector([type="number"]);
+
+
