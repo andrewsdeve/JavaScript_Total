6e0854c0a626cd1990057287c5453ec9823a1742
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-09 05:05:48 -0500

    dom1

--- Dia_12/RepasoDOM/app.js
+++ Dia_12/RepasoDOM/app.js
@@ -1,2 +1,7 @@
-const body = document.body.childNodes;
-console.log(body);
+const wrapper = document.body.children[0];
+const div = wrapper.children[1];
+const h1 = div.previousElementSibling;
+const anchor = div.children[1];
+anchor.addEventListener('click', function(){
+    h1.textContent = "Estoy siendo Modificado"
+})
