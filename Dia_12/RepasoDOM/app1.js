22a1b46457601cfd6fd48c480936f9f7cc90fe2f
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-09 06:05:49 -0500

    Fin

--- Dia_12/RepasoDOM/app1.js
+++ Dia_12/RepasoDOM/app1.js
@@ -3,4 +3,9 @@ const title = document.querySelectorAll('.title');
 const link = document.querySelector('.link.bold');
 const input = document.querySelector([type="number"]);
 
+const img = document.querySelector('img');
+img.src ='https://i.ebayimg.com/images/g/OQMAAOSwbdpWUNl7/s-l960.jpg'
+
+
+
 
