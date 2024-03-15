6ed49c1799fce4c42bb8ea3e23ff7470e6b5b509
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-15 06:28:04 -0500

    Ejercicio2

--- Dia_13/10-Ejercicios/promesas1.js
+++ Dia_13/10-Ejercicios/promesas1.js
@@ -14,3 +14,17 @@ function test(resuelto){
 }
 
 /* Programar la función getDatos() para que retorne una promesa (new Promise) con el resultado de realizar una petición (utilizando XMLHttpRequest) sobre la url que recibe como parametro. Segun el estado de la peticion, recordar que se deben utilizar los métodos resolve (que devuelve el resultado de la peticion) y el método reject (que devuelve el error). */
+
+function getDatos(){
+    return new Promise(function(resolve,reject){
+        let xhr = new XMLHttpRequest('GEL','https://jsonplaceholder.typicode.com/users');
+        xhr.onload = function(){
+            if(xhr.status === 200){
+                resolve(JSON.parse(xhr.responseText));
+            }else{
+                reject(xhr.statusText)
+            }
+        }
+        xhr.send()
+    })
+}
