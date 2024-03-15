16b0ee7bf21a4d18498ebe62c7e18d15151a9d18
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-15 05:28:35 -0500

    Ejercicio1

---
+++ Dia_13/10-Ejercicios/promesas1.js
@@ -0,0 +1,5 @@
+/* Dentro de la función test, asignar a la variable promesa, un nuevo objeto Promise (promesa) que según la variable resultado (boolean), si la misma es true, llame al método resolve pasando el parámetro "Correcto" y en caso contrario (false), llamar al método reject pasando el parámetro "Error". */
+
+let promesa = new Promise(function(resolve,reject){
+    
+})
