a24817b46a38605ee0c9b53b38949006e72949bb
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-15 05:10:05 -0500

    PromesasFIN

---
+++ Dia_13/03-Promesas/promesa_2.js
@@ -0,0 +1,19 @@
+// EJEMPLO DE PROMESA # 2 cuando se tienen dos promesas
+
+let promesa1 = new Promise(function(resolve,reject){
+    setTimeout(resolve,2000,'Hola');
+});
+
+
+let promesa2 = new Promise(function(resolve,reject){
+    setTimeout(resolve,4000,'Mundo');
+});
+
+Promise.all([promesa1,promesa2]).then(function(valores){
+    console.log(valores)
+
+});
+
+console.log('El codigo sigue su camino');
+
+
