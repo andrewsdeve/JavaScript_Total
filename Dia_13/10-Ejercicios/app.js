05f8db9a4af3c5146188a20eb1015bc5f28072da
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-15 04:31:58 -0500

    SegundoEjercicio

--- Dia_13/10-Ejercicios/app.js
+++ Dia_13/10-Ejercicios/app.js
@@ -12,3 +12,19 @@ function imprimir(){
 }
 
 test(imprimir)
+
+// EJERCICIO # 2
+
+/* Programar la función test que dispare un timeout con un tiempo de 1500, y que cumplido el tiempo, llame a una función callback que también se deberá programar.  Esta segunda función, se debe llamar mensaje() y deberá mostrar en un alert lo siguiente: Mensaje impreso */
+
+function test(callback){
+    setTimeout(function(){
+        callback();
+    },1500)
+}
+
+function mensaje(){
+    alert('Mesaje Impreso');
+};
+
+test(mensaje);
