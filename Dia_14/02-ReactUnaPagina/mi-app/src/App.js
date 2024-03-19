4a9264952a65da03698e263898f236b6a9548331
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-19 05:36:13 -0500

    FinReact

--- Dia_14/02-ReactUnaPagina/mi-app/src/App.js
+++ Dia_14/02-ReactUnaPagina/mi-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Saludo from './saludo';
+import Mensaje from './Mensaje';
 
 function App(){
   return(
     <div>
       <Saludo nombre = 'Juan'/>
+      <Mensaje mensaje = "Bienbenido a mi App React"/>
     </div>
   )
 }
---
+++ Dia_14/02-ReactUnaPagina/mi-app/src/Mensaje.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Mensaje(props){
+    return <p>{props.mensaje}</p>;
+}
+
+export default Mensaje;
+
