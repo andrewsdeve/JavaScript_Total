2d895269337a1fd4658c82e5d49a1618e3994b61
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-21 10:15:31 -0500

    PUT

--- Dia_15/02-Otras Solicitudes/app.js
+++ Dia_15/02-Otras Solicitudes/app.js
@@ -7,4 +7,16 @@
 5. PATCH => Actualiza parcialmente un recurso en el servidor
 */
 
-
+fetch('https://jsonplaceholder.typicode.com/posts/5', {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        title: 'Nuevo titulo',
+        body: 'Nueva descripcion'
+    })
+})
+.then(respuesta => respuesta.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
