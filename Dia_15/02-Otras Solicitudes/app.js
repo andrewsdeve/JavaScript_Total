687cec6bf393732dc22606106523cf879403df60
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-21 10:21:40 -0500

    Metodos

--- Dia_15/02-Otras Solicitudes/app.js
+++ Dia_15/02-Otras Solicitudes/app.js
@@ -7,6 +7,8 @@
 5. PATCH => Actualiza parcialmente un recurso en el servidor
 */
 
+
+/* 
 fetch('https://jsonplaceholder.typicode.com/posts/5', {
     method: 'PUT',
     headers: {
@@ -15,6 +17,32 @@ fetch('https://jsonplaceholder.typicode.com/posts/5', {
     body: JSON.stringify({
         title: 'Nuevo titulo',
         body: 'Nueva descripcion'
+    })
+})
+.then(respuesta => respuesta.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
+
+*/
+
+
+/* 
+fetch('https://jsonplaceholder.typicode.com/posts/5', {
+    method: 'DELETE',
+    })
+.then(respuesta => respuesta.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
+*/
+
+
+fetch('https://jsonplaceholder.typicode.com/posts/5', {
+    method: 'PATCH',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        title: 'Nuevo titulo'
     })
 })
 .then(respuesta => respuesta.json())
