0b0e6704e67a36a504e46ae8d80a21d02d5c95ed
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-03-21 09:59:28 -0500

    MasMetodos

---
+++ Dia_15/02-Otras Solicitudes/app.js
@@ -0,0 +1,10 @@
+/* Solicitudes adicionales que pueden servir para hacer conexion con el servidor
+
+1. GET => bUSCAR INFORMACION
+2. POST => Grabar Informacion
+3. PUT => Reemplazar o Actualizar
+4. DELETE => Cortar y elminar recursos en el servidor
+5. PATCH => Actualiza parcialmente un recurso en el servidor
+*/
+
+
